54efcac4f406576bbda10fa13d17fe809b36ca88
CadenCoveney <cannottouchgrass@gmail.com>
2024-10-01 21:42:59 +0000

    Updated primm_11.py

--- primm_11.py
+++ primm_11.py
@@ -5,11 +5,11 @@ Name - Date
 """
 
 def main():
-  
-    num1: int = input("Enter a number: ")
-    num2: int = input("Enter another number: ")
+      # takes the two numbers and adds them together.
+    num1: int = int(input("Enter a number: "))
+    num2: int = int(input("Enter another number: "))
     total: int = num1+num2
-
+      # prints the numbers and the answer.
     print(f"{num1} + {num2} = {total}")
 
 if __name__ == "__main__":
